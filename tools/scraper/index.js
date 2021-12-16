094a174c3b9ed9f78d6e6ee3f54b9c235edd9c04
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2021-12-16 15:25:43 +0100

    Scrap the all comunes

--- tools/scraper/index.js
+++ tools/scraper/index.js
@@ -1,15 +1,15 @@
 import { select } from 'd3-selection'
 import { parse } from 'node-html-parser'
 import get from './get.js'
-// const save = require('./save')
 import config from './.config.js'
-const { host } = config
+const { host, path } = config
 
-if (true) {
-  const path = '/cap-regioni-italiane'
-
+// All pages we need on the host are formatted almost the same way.
+// We use one function to parse it
+async function getCodes(path) {
   let page = await get(host, path)
   page = parse(page)
+  const data = []
 
   const tr = select(page)
     .select('tbody')
@@ -17,21 +17,44 @@ if (true) {
     .filter(function () { return select(this).selectAll('td').size() == 2})
 
   tr.each(function () {
-    let name, codes, path
+    let name, codes, path, a
     select(this)
       .selectAll('td')
       .each(function (ignore, i) {
+        let el = select(this)
+        let a = el.select('a')
+        if (!a.empty()) {
+          el = a
+        }
         switch (i) {
           case 0:
-            name = select(this).html()
+            name = el.html()
             break
           case 1:
-            const a = select(this).select('a')
             path = a.attr('href')
             codes = a.html()
             break
         }
       })
-    console.log(name, codes, path)
+    data.push({ name, codes, path })
   })
+
+  return data
 }
+
+const regions = await getCodes(path)
+
+await Promise.all(regions.map(async region => {
+  const provinces = await getCodes(region.path)
+  region.provinces =  provinces
+  delete region.path
+
+  await Promise.all(provinces.map(async province => {
+    const comunes = await getCodes(province.path)
+    comunes.forEach(comune => delete comune.path)
+    province.comunes = comunes
+    delete province.path
+  }))
+}))
+
+console.log(JSON.stringify(regions, null, 2))
