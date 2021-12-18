2355f0aa782cb3cf415c3e94d0250fa7536eef61
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2021-12-18 17:00:48 +0100

    Fix: parser can't find all tables

--- tools/scraper/index.js
+++ tools/scraper/index.js
@@ -1,47 +1,45 @@
-import { select } from 'd3-selection'
-import { parse } from 'node-html-parser'
+//import { select } from 'd3-selection'
+// import { parse, valid } from 'node-html-parser'
+import { parseDocument as parse } from 'htmlparser2'
+import { getElementsByTagName as select, textContent } from 'domutils'
 import get from './get.js'
 import config from './.config.js'
 const { host, path } = config
 
+
+// Helper
+// Traverse all children till no tags left. Returns the last element
+const skipTags = e => {
+  const tag = e.children.find(e => e.type == 'tag')
+  return tag ? skipTags(tag) : e
+}
+
+// Helper
+// Returns true if the element has given class
+const classed = className => e => e.attribs.class.includes(className)
+
 // All pages we need on the host are formatted almost the same way.
 // We use one function to parse it
 async function getCodes(path) {
-  let page = await get(host, path)
-  page = parse(page)
   const data = []
 
-  const tr = select(page)
-    .select('tbody')
-    .selectAll('tr')
-    .filter(function () { return select(this).selectAll('td').size() == 2})
+  let page = await get(host, path)
+  page = parse(page)
 
-  tr.each(function () {
-    let name, codes, path, a
-    select(this)
-      .selectAll('td')
-      .each(function (ignore, i) {
-        let el = select(this)
-        const a = el.select('a')
-        if (!a.empty()) { el = a }
-        let strong = el.select('strong')
-        if (!strong.empty()) { el = strong }
-        switch (i) {
-          case 0:
-            name = el.html()
-            break
-          case 1:
-            path = a.attr('href')
-            codes = el.html()
-            break
-        }
-      })
-    data.push({ name, codes, path })
-  })
+  const tables = select('table', page).filter(classed('table-bordered'))
+
+  select('tr', select('tbody', tables))
+    .map(tr => select('td', tr))
+    .filter(tds => tds.length == 2)
+    .forEach(tds => {
+      const name = textContent(skipTags(tds[0]))
+      const codes = textContent(skipTags(tds[1]))
+      const path = select('a', tds[1]).map(a => a.attribs.href)[0]
+      data.push({ name, codes, path })
+    })
 
   return data
 }
-
 
 async function getComunes(province) {
   const comunes = await getCodes(province.path)
@@ -51,14 +49,15 @@ async function getComunes(province) {
 async function getProvinces(region) {
   const provinces = await getCodes(region.path)
   region.provinces =  provinces
-  //await Promise.all(provinces.map(getProvince))
 }
 
 const regions = await getCodes(path)
 await Promise.all(regions.map(getProvinces))
 await Promise.all(regions.map(({ provinces }) => Promise.all(provinces.map(getComunes))))
 
+// Remove url
 const noPath = o => delete o.path
+
 regions.forEach(region => {
   noPath(region)
   region.provinces.forEach(province => {
