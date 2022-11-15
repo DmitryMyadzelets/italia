b85caf1ded1801737668912c4b7a925a137bab9a
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2022-11-15 19:19:42 +0100

    Convert to ESM

--- tools/convert.js
+++ tools/convert.js
@@ -1,20 +1,23 @@
-const path = require("path")
-const csvtojson = require("csvtojson")
-const save = require("./save")
+import path from 'path'
+import csvtojson from 'csvtojson'
+import save from './save.js'
+import { fileURLToPath } from 'url'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const config = {
-  src: "../csv/data.csv",
-  dst: "../json/data.json",
+  src: '../csv/data.csv',
+  dst: '../json/data.json',
 }
 
 const options = {
-  delimiter: ";",
+  delimiter: ';',
   trim: true
 }
 
 ;(async () => {
-  const src = path.resolve(__dirname, config.src)
-  const dst = path.resolve(__dirname, config.dst)
+  const src = path.resolve(dirname, config.src)
+  const dst = path.resolve(dirname, config.dst)
 
   const array = await csvtojson(options).fromFile(src)
 
